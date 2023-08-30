c7bd336e8768621d0b0044d97c61f45e2d85ef5e
jisan441 <64075088+jisan441@users.noreply.github.com>
2023-08-30 17:02:27 +0530

    Login page is done.

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var userHelper = require('../helper/user-helper')
+const async = require('hbs/lib/async')
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('userview/user-view', { title: 'Express' });
+  res.render('userview/user-view');
 });
 
 module.exports = router;
