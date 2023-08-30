db4767e337f17f440074c401d055e6dbf62511f1
jisan441 <64075088+jisan441@users.noreply.github.com>
2023-08-30 17:27:05 +0530

    user inputs get from the form.

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,31 @@ const async = require('hbs/lib/async')
 
 
 /* GET home page. */
+var varifyLogin = (req, res, next) => {
+  if (req.session.user) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
 router.get('/', function (req, res, next) {
-  res.render('userview/user-view');
+  res.render('userview/login');
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.user) {
+    res.render('userview/main-view')
+  } else {
+    res.render('user/login', { loginErr: req.session.userloginErr })
+    req.session.userloginErr = false;
+  }
+})
+
+router.get('/signup', (req, res) => {
+  res.render('userview/signup')
+})
+
+router.post('/loginaction', (req, res) => {
+  console.log(req.body)
+})
 module.exports = router;
